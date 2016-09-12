55d806437167134d347a351c25ff3badb72b1e2f
Peter Hickman <peterhickman386@googlemail.com>
2016-09-12 15:51:14 +0100

    maybe we dont need runsudo after all

--- scripts/package.sh
+++ scripts/package.sh
@@ -6,4 +6,8 @@ ACTION=$1
 
 PACKAGE=$2
 
-apt-get $ACTION -y -q -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" $PACKAGE
+if [ "$(id -u)" != "0" ]; then
+	sudo apt-get $ACTION -y -q -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" $PACKAGE
+else
+	apt-get $ACTION -y -q -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" $PACKAGE
+fi
