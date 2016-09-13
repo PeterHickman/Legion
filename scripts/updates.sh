fd69e11ae92c25c1465e79146a5740d0130607c2
Peter Hickman <peter@abelsoninfo.com>
2016-09-13 12:52:38 +0100

    handle sudo better

--- scripts/updates.sh
+++ scripts/updates.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+if [ "$(id -u)" != "0" ]; then
+  SUDO_PREFIX='sudo'
+else
+  SUDO_PREFIX=''
+fi
+
 export DEBIAN_FRONTEND=noninteractive
 
-apt-get update -q
-apt-get dist-upgrade -q -y
+$SUDO_PREFIX apt-get update -q
+$SUDO_PREFIX apt-get dist-upgrade -q -y
