824629bc953edb062c987b40a0774b9ad5858415
Peter Hickman <peterhickman386@googlemail.com>
2017-02-12 19:24:10 +0000

    not really anything to do with legion

--- scripts/bootstrap/hostname.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-HOSTNAME=$1
-
-hostname $HOSTNAME
-
-echo $HOSTNAME > /etc/hostname
-
-echo Hostname is $HOSTNAME
--- scripts/bootstrap/timezone.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-ZONE=$1
-
-echo $ZONE > /etc/timezone
-rm /etc/localtime
-ln -s /usr/share/zoneinfo/$ZONE /etc/localtime
-
-echo Timezone is now $ZONE
