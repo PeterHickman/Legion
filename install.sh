8c81cf2c3a58f3bdde083fbebbddf3fb5f5aa750
Peter Hickman <peter@abelsoninfo.com>
2017-11-30 14:53:59 +0000

    start of a man page

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-WHERE='/usr/local/bin/'
+BINARY='/usr/local/bin/'
+MAN='/usr/local/share/man/man1/'
 
-echo "Installing legion to $WHERE"
+echo "Installing legion to $BINARY"
 
-install -v legion $WHERE
+install -v legion $BINARY
+
+echo "Installing man page to $MAN"
+
+install -v legion.1 $MAN
--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-WHERE='/usr/local/bin/'
+BINARY='/usr/local/bin/'
+MAN='/usr/local/share/man/man1/'
 
-echo "Removing legion from $WHERE"
+echo "Removing legion from $BINARY"
 
-rm -f $WHERE/legion
+rm -f $BINARY/legion
+
+echo "Removing man page from $MAN"
+
+rm -f $MAN/legion.1
