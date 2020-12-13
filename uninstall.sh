a17d8f70153655c2343fa94970bd2adfc9377090
Peter Hickman <peterhickman386@googlemail.com>
2020-12-13 15:35:05 +0000

    no man page for the present

--- uninstall.sh
+++ uninstall.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
 BINARY='/usr/local/bin/'
-MAN='/usr/local/share/man/man1/'
 
 echo "Removing legion from $BINARY"
 
 rm -f $BINARY/legion
-
-echo "Removing man page from $MAN"
-
-rm -f $MAN/legion.1
