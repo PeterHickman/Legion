9558b927a20b500493f2292dd342a8ec455e98f9
Peter Hickman <peterhickman386@googlemail.com>
2020-12-13 15:34:48 +0000

    no man page for the present

--- install.sh
+++ install.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
 BINARY='/usr/local/bin/'
-MAN='/usr/local/share/man/man1/'
 
 echo "Installing legion to $BINARY"
 
 install -v legion $BINARY
-
-echo "Installing man page to $MAN"
-
-install -v legion.1 $MAN
